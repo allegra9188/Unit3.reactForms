b30c15a6fead5617ee3f436d190afd8891ca9265
Allegra McDermott <allegramcdermott@allegras-mbp.mynetworksettings.com>
2023-10-15 21:09:57 -0400

    everything prior to wrapping up step added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import SignUpForm from './components/SignUpForm'
-import './App.css'
+import { useState } from "react";
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
+import "./App.css";
 
 function App() {
-  
-
+  const [token, setToken] = useState(null);
   return (
     <>
-      
+      <SignUpForm token={token} setToken={setToken}/>
+      <Authenticate token={token} setToken={setToken}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
